3be42534c9d9d5af2a4cd4126a69ec42a7d84f5d
th-shivam <dreamyshivam01@gmail.com>
2026-02-05 23:06:43 +0530

    Understanding the react

---
+++ Coder_Army_React/Day_2/main.js
@@ -0,0 +1,50 @@
+const element1 = React.createElement(
+  "h1",
+  { className: "greeting" },
+  "Hello, world!",
+);
+
+const element2 = React.createElement(
+  "h1",
+  { className: "greeting" },
+  "I am Hacker ",
+);
+
+ReactDOM.render(element1, document.getElementById("root"));
+
+ReactDOM.render(element2, document.getElementById("root"));
+
+// when we try to insert two element at same time then last element will be inserted
+// jab ham apne khud local machine me code likh ke dekhe hai react ka tb wo hme ek element return kr rha tha
+// but original react element nhi return krta hai , wo sirf ek object return krta hai
+// jise react DOM apne virtual DOM me store krta hai
+// aur phir virtual DOM ko real DOM me update krta hai
+
+const React = {
+  createElement: function (type, props, ...children) {
+    return {
+      type: type,
+      props: {
+        ...props,
+        children: children,
+      },
+    };
+  },
+};
+
+const reactDom = {
+  render: function (reactElement, root) {
+    const element = document.createElement(reactElement.type);
+    const { props } = reactElement;
+    for (const key in props) {
+      if (key === "style") {
+        Object.assign(element.style, props);
+      } else if (key === "children") {
+        element.textContent = props[key];
+      } else {
+        element[key] = props[key];
+      }
+    }
+    root.appendChild(element);
+  },
+};
