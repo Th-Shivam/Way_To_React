add00edc87da91debc9e740ebf79f9039147288c
th-shivam <dreamyshivam01@gmail.com>
2026-02-11 11:27:51 +0530

    Creating a react app using vite

---
+++ Coder_Army_React/Day_4.2/src/App.jsx
@@ -0,0 +1,28 @@
+// default export 
+export default function App() {
+
+  return (
+    <>
+      <h1>hello , im from default export  </h1>
+    </>
+  )
+}
+
+// named export 
+export function Shivam() {
+  return (
+    <>
+      <h1> hello , im Shivam from named export </h1>
+    </>
+  )
+}
+
+// named export 
+export function Singh() {
+  return (
+    <>
+      <h1> hello , im Singh from named export </h1>
+    </>
+  )
+}
+
